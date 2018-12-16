02e47c1023629030b85fc5f63a1977ce376a3b8d
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2018-12-15 19:09:43 -0500

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+ANSERINI="git/Anserini-Spark/target/anserini-spark-0.0.1-SNAPSHOT-fatjar.jar"
+INDEX="lucene-index.core18.pos+docvectors+rawdocs-10"
+PY_FILES="src/spacy_ner.py,src/allen_ner.py"
+
 spark-2.4.0-bin-hadoop2.7/bin/spark-submit \
 	--master "local[*]" \
-	--py-files src/spacy_ner.py \
-	--jars git/Anserini-Spark/target/anserini-spark-0.0.1-SNAPSHOT-fatjar.jar \
-	src/main.py --index ~/lucene-index.wash18.pos+docvectors+rawdocs
+	--py-files $PY_FILES --jars $ANSERINI src/main.py --index $INDEX

aac25f7d5c07efd634c6b76ceefc3bdadec9c7c5
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-01-24 19:41:00 -0500

    NLTK sentence segmenter

--- libs/nltk/seg.py
+++ libs/nltk/seg.py
@@ -1,10 +1,17 @@
+import nltk
+
 from ..task import Task
 
 
 class NLTKSentenceSegmenter(Task):
 
     def __init__(self, config):
-        print(config)
+        self.config = config
+        nltk.download("punkt")
 
     def run(self, data):
-        print(data)
+        paragraphs = [], words = 0
+        for paragraph in data:
+            paragraphs.append(nltk.sent_tokenize(paragraph))
+            words += len(nltk.word_tokenize(paragraph))
+        return paragraphs, 0

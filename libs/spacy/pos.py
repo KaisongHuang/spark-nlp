759675a0b29691b0b44f2a5e7cf301bfffd06500
Ryan Clancy <ryan.clancy@uwaterloo.ca>
2019-01-24 19:10:49 -0500

    spaCy Part-of-Speech tagging

--- libs/spacy/pos.py
+++ libs/spacy/pos.py
@@ -1,10 +1,23 @@
+import spacy
+
 from ..task import Task
 
 
 class SpacyPartOfSpeechTagger(Task):
 
     def __init__(self, config):
-        print(config)
+        self.config = config
+        self.nlp = spacy.load("en")
 
     def run(self, data):
-        print(data)
+        paragraphs = []
+        words = 0
+        for paragraph in self.nlp.pipe(data):
+            sentences = []
+            for sentence in paragraph.sents:
+                tokens = []
+                for token in sentence:
+                    tokens.append("{}[{}]".format(token.text, token.pos_))
+                sentences.append(" ".join(str(t) for t in tokens))
+            paragraphs.append(sentences)
+        return paragraphs, words

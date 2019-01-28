6c7084183a847dcad9c8e418a558a7b5ecf02050
KaisongHuang <k86huang@uwaterloo.ca>
2019-01-28 02:16:41 -0500

    load corenlp jars

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # The Anserini-Spark JAR
-ANSERINI="/home/raclancy/anserini-spark-0.0.1-SNAPSHOT-fatjar.jar"
+ANSERINI="/home/k86huang/anserini-spark-0.0.1-SNAPSHOT-fatjar.jar"
+
+# The Stanford CoreNLP JAR
+CORENLP="/home/k86huang/stanford-corenlp-3.9.2.jar"
+
+# The Stanford CoreNLP English Model JAR
+COREMODEL="/home/k86huang/stanford-corenlp-3.9.2-models.jar"
 
 # The Lucene index directory
 INDEX="hdfs://node-master:9000/indexes/core18-1000"
@@ -17,4 +23,4 @@ spark-submit \
 	--num-executors 9 --executor-cores 16 --executor-memory 32G --driver-memory 32G \
 	--conf "spark.yarn.appMasterEnv.PYSPARK_PYTHON=$PYSPARK_PYTHON" \
 	--archives "venv.zip#venv" \
-	--py-files $PY_FILES --jars $ANSERINI main.py --index $INDEX --library spacy --task pos --num 10
+	--py-files $PY_FILES --jars $ANSERINI,$CORENLP,$COREMODEL main.py --index $INDEX --library spacy --task pos --num 10

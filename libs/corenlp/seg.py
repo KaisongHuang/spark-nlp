7433f1f660a7309e9d68bacd496f414a063adb8b
KaisongHuang <k86huang@uwaterloo.ca>
2019-01-28 02:17:29 -0500

    CoreNLP Seg

--- libs/corenlp/seg.py
+++ libs/corenlp/seg.py
@@ -3,8 +3,28 @@ from ..task import Task
 
 class CoreNLPSentenceSegmenter(Task):
 
-    def __init__(self, config):
-        print(config)
+    def __init__(self, spark_context):
+        self.sc = spark_context
+
+        # Get an instance of the Properties
+        self.props = self.sc._jvm.java.util.Properties()
+        self.setProperty("annotators", "tokenize")
+        self.setProperty("ner.useSUTime", "false");
+
+        # Get an instance of the Pipeline
+        self.pipeline = self.sc._jvm.edu.stanford.nlp.pipeline.StanfordCoreNLP(self.props)
 
     def run(self, data):
-        print(data)
+        paragraphs = []
+        words = 0
+
+        for paragraph in data:
+            sentences = []
+            anno = self.sc._jvm.edu.stanford.nlp.pipeline.Annotation(paragraph)
+            self.pipeline.annotate(anno)
+            sent_anno = self.sc._jvm.edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation()
+            for sentence in anno.get(sent_anno.getClass()):
+                sentences.append(str(sentence))
+                words += len(sentence)
+            paragraphs.append(sentences)
+        return paragraphs, words

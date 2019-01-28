c9a0366a6a7cf9a46675f94554a5058975328825
KaisongHuang <k86huang@uwaterloo.ca>
2019-01-28 02:17:41 -0500

    CoreNLP POS

--- libs/corenlp/pos.py
+++ libs/corenlp/pos.py
@@ -3,8 +3,35 @@ from ..task import Task
 
 class CoreNLPPartOfSpeechTagger(Task):
 
-    def __init__(self, config):
-        print(config)
+    def __init__(self, spark_context):
+        self.sc = spark_context
+
+        # Get an instance of the Properties
+        self.props = self.sc._jvm.java.util.Properties()
+        self.setProperty("annotators", "tokenize,ssplit")
+        self.setProperty("ner.useSUTime", "false");
+
+        # Get an instance of the Pipeline
+        self.pipeline = self.sc._jvm.edu.stanford.nlp.pipeline.StanfordCoreNLP(self.props)
 
     def run(self, data):
-        print(data)
+        paragraphs = []
+        words = 0
+
+        for paragraph in data:
+            sentences = []
+            anno = self.sc._jvm.edu.stanford.nlp.pipeline.Annotation(paragraph)
+            self.pipeline.annotate(anno)
+            sent_anno = self.sc._jvm.edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation()
+
+            for sentence in anno.get(sent_anno.getClass()):
+                doc = self.sc._jvm.edu.stanford.nlp.pipeline.CoreDocument(sentence)
+                self.pipeline.annotate(doc)
+                tokens = doc.tokens()
+
+                for token in tokens:
+                    pos_anno = self.sc._jvm.edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation()
+                    sentences.append(" ".join("{}[{}]".format(token, token.get(pos_anno.getClass()))))
+                words += len(tokens)
+
+        return paragraphs, words

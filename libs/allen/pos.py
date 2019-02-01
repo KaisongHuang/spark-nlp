0204ef44314be1cca2eb5d6a876b81df7ef18203
KaisongHuang <k86huang@uwaterloo.ca>
2019-02-01 01:26:52 -0500

    Implement AllenNLP POS

--- libs/allen/pos.py
+++ libs/allen/pos.py
@@ -1,10 +1,39 @@
 from ..task import Task
+from allennlp.predictors.predictor import Predictor
+from spacy import load
 
 
 class AllenNLPPartOfSpeechTagger(Task):
 
     def __init__(self, gpu):
-        print(gpu)
+        self.gpu = gpu
+        self.nlp = load("en", disable=["tagger", "ner"])
+        self.predictor = Predictor.from_path(
+            "https://s3-us-west-2.amazonaws.com/allennlp/models/biaffine-dependency-parser-ptb-2018.08.23.tar.gz")
 
     def run(self, data):
-        print(data)
+        results = []
+        words = 0
+
+        # For each parsed doc...
+        for doc in self.nlp.pipe(data):
+
+            par = []
+
+            batch = [{"sentence": sentence.text} for sentence in doc.sents]
+
+            try:
+                predictions = self.predictor.predict_batch_json(batch)
+            except:
+                continue
+
+            for prediction in predictions:
+                words += len(prediction["words"])
+                par.append({
+                    "text": prediction["words"],
+                    "pos": prediction["pos"]
+                })
+
+            results.append(par)
+
+        return results, words
